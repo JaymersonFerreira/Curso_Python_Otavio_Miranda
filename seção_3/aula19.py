768ffaa1eb5753ce23c4b0b9efed182a45a7a82c
Jaymerson Ferreira <jaymerson.ferreira.s@gmail.com>
2023-10-04 10:23:08 -0300

    solução aula 26

--- seção_3/aula19.py
+++ seção_3/aula19.py
@@ -5,3 +5,4 @@ if primeiro_valor >= segundo_valor:
     print(f'{primeiro_valor=} é maior ou igual ao {segundo_valor=}.')
 else: 
     print(f'{segundo_valor=} é maior.')
+
---
+++ seção_3/aula24.py
@@ -0,0 +1,6 @@
+nome = 'Lucas'
+if '5' in nome:
+    print('Esta dentro')
+elif 'b' not in nome:
+    print('Não esta dentro')
+print('as' in nome)
